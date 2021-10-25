759afcea19d6ab552f9893bc8280fb4ddbe5caaf
Poulenas <Poulenas@LAPTOP-LHDB2PDG>
2021-10-25 12:54:30 +0200

    Super puissance 4 1.0 Début vide

---
+++ SP4_console_POULENAS/src/sp4_console_poulenas/Joueur.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_poulenas;
+
+/**
+ *
+ * @author Poulenas
+ */
+public class Joueur {
+    int nom;
+    String couleur;
+}
---
+++ SP4_console_POULENAS/src/sp4_console_poulenas/SP4_console_POULENAS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_poulenas;
+
+/**
+ *
+ * @author Poulenas
+ * Poulenas Sandra
+ * TDA 
+ * Super puissance 4 
+ * 25.10.2021
+ */
+public class SP4_console_POULENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
