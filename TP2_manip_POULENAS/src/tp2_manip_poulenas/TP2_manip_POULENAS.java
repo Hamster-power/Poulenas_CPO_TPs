7cffa8caedd864ca9c9c26218cd5039fa57a1d94
Poulenas <Poulenas@LAPTOP-LHDB2PDG>
2021-11-01 22:04:51 +0100

    TP2 exercice 3 fini

--- TP2_manip_POULENAS/src/tp2_manip_poulenas/TP2_manip_POULENAS.java
+++ TP2_manip_POULENAS/src/tp2_manip_poulenas/TP2_manip_POULENAS.java
@@ -21,9 +21,34 @@ public class TP2_manip_POULENAS {
         Tartiflette assiette1 = new Tartiflette(500) ;
         Tartiflette assiette2 = new Tartiflette(600) ;
         Tartiflette assiette3 = assiette2 ; 
+        //System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        //System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+        //assiette2.nbCalories=800;
+        //System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        //System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+        // On voit qu'en rélité assiette2 et assiette3 sont la même chose puisque dès quenl'on 
+        // change une cjose dans assiette2 elle se modifie aussi dans assiette3
+        Tartiflette assietteTemp = new Tartiflette(300);
+        assietteTemp.nbCalories = assiette2.nbCalories;
+        assiette2.nbCalories=assiette1.nbCalories;
+        assiette1.nbCalories=assietteTemp.nbCalories;
+        System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories) ;
         System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
-        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;
-
+        //Moussaka assiette666 = assiette1 ; 
+        //Moussaka assiette667 = new Tartiflette() ;
+        // Aucune des 2 lignes n'est correcte car assiette1 est une Tartiflette et donc assiette 666
+        // qui est une Moussaka ne peut pas être un Tatriflette. Même chose assiette667 est déclaré comme
+        // Moussaka et ne peut donc pas être une nouvelle Tartiflette !
+        Moussaka Tableau [];
+        Tableau = new Moussaka [10];
+        int Calories = 0;
+        for (int i=0; i<10;){
+            Tableau[i]=new Moussaka(Calories);
+            Calories = Calories+100;
+            System.out.println("Moussaka"+i+" contient "+Tableau[i].nbCalories+" Calories");
+            i=i+1;
+        }
+        
     }
     
 }
